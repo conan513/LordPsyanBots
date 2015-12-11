6eba53f5b68ce9e2577045fc3f38f58be38bde58
LordPsyan <realmsofwarcraft@gmail.com>
2015-12-11 07:19:59 -0600

    Small update to set max level to 120, and remove the checks for max level in world.cpp. You can now set any level up to 255 (core limitation. Will eventually find a way around this).

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -763,13 +763,13 @@ void World::LoadConfigSettings(bool reload)
     }
     else
         m_int_configs[CONFIG_MAX_PLAYER_LEVEL] = sConfigMgr->GetIntDefault("MaxPlayerLevel", DEFAULT_MAX_LEVEL);
-
+/* Removed so max player level can be above 100. WARNING: still causes druid crash after level 100.
     if (m_int_configs[CONFIG_MAX_PLAYER_LEVEL] > MAX_LEVEL)
     {
         TC_LOG_ERROR("server.loading", "MaxPlayerLevel (%i) must be in range 1..%u. Set to %u.", m_int_configs[CONFIG_MAX_PLAYER_LEVEL], MAX_LEVEL, MAX_LEVEL);
         m_int_configs[CONFIG_MAX_PLAYER_LEVEL] = MAX_LEVEL;
     }
-
+*/
     m_int_configs[CONFIG_MIN_DUALSPEC_LEVEL] = sConfigMgr->GetIntDefault("MinDualSpecLevel", 40);
 
     m_int_configs[CONFIG_START_PLAYER_LEVEL] = sConfigMgr->GetIntDefault("StartPlayerLevel", 1);
